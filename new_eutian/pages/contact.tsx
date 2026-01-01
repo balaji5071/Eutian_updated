d14ed2a0619905a2fca3e11f8deb2af27ab86e4e
balaji5071 <balajich058@gmail.com>
2026-01-01 13:20:35 +0530

    done

--- new_eutian/pages/contact.tsx
+++ new_eutian/pages/contact.tsx
@@ -224,7 +224,7 @@ export default function Contact() {
 																{option}
 															</SelectItem>
 														))}
-													</SelectContent>
+													</SelectContent>  
 												</Select>
 												<FormMessage />
 											</FormItem>
